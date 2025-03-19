2546a0f7da482183bc644af5fb567c2121a5b803
Shubham kahar <Shubhamkahar196@gmail.com>
2025-03-19 20:09:27 +0530

    adding index.js

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,3 +1,19 @@
 // import dotenv 
 import dotenv from 'dotenv';
 
+// import connectDB form db folder index.js
+import connectDB from './db/index.js';
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
+.then( ()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) =>{
+    console.log("Mongodb connection fail", err);
+})
