c451ea421dcf9fb6894606eb62cb1d3f5c83ca72
Shubham kahar <Shubhamkahar196@gmail.com>
2025-03-23 19:39:36 +0530

    Adding habit controller

---
+++ Backend/src/controllers/habit.controller.js
@@ -0,0 +1,38 @@
+import { asyncHandler } from "../utils/asyncHandler";
+import Habit from '../models/habit.models.js'
+
+// create habit
+const createHabit = asyncHandler(async (req,res)=>{
+    const habit = await Habit.create(req.body);
+    res.status(201).json({
+        message: "Habit created successfully",Habit
+    });
+});
+
+// get habit
+
+const getHabit = asyncHandler(async (req,res)=>{
+    const habit = await Habit.find().populate("user");
+    res.status(200).json({
+        message: "Habits retrieved Successfully",habit
+    })
+})
+
+//update habit
+const updateHabit = asyncHandler(async(req,res)=>{
+    const habit = await Habit.findByIdAndUpdate(req.params.id,req,body,{new: true});
+    res.status(200).json({
+        message: "Habit updated successfully", habit
+    });
+})
+
+// delete habit
+const deleteHabit = asyncHandler(async(req,res)=>{
+    await Habit.findByIdAndDelete(req.params.id);
+    res.status(200).json({
+        message: "Habit deleted successfully"
+    });
+})
+
+//exporting
+export {createHabit, getHabit, updateHabit, deleteHabit};
